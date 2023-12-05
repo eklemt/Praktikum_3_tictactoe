0cf7bb4f5910ffc1b2234cd29cfda2223852a33f
Caro <103448962+CaroCarocaestchen@users.noreply.github.com>
2023-12-05 16:33:07 +0100

    Update Praktikum_3_tictactoe.c
    Fertige Version ohne schweren Computer

--- Praktikum_3_tictactoe.c
+++ Praktikum_3_tictactoe.c
@@ -21,105 +21,131 @@ Version:		1
 short einlesenEinerZahl(char text[], short min, short max);		// Funktion, um eine Zahl einzulesen
 bool feldIstBelegt(char spielfeld[3][3], short zeile, short spalte); //Funktion, die überprüft, ob ein Feld belegt ist 
 void einfacherComputergegner(char spielfeld[3][3]);					// computer soll random ein feld auswählen und belegen
-void gibDasAktuelleSpielfeldAus(char spielfeld[3][3]); 		// Funktion, die das aktuelle Spielfeld ausgibt 
+void gibDasAktuelleSpielfeldAus(char spielfeld[3][3], int anzahlRunden, int spielstandComputer, int spielstandMensch); 		// Funktion, die das aktuelle Spielfeld ausgibt 
 bool gewinnErmitteln(char spielfeld[3][3]);				// Funktion, die überprüft, ob es einen Gewinner gibt 
 
 int main() {
-	char spielfeldArray[3][3]; // Beinhaltet alle 9 Felder des Spielfeldes 
-	bool spielerIstDran = true; // Hier hätte ich eigentlich lieber einen boolean, mit spielerbeginnt true oder false 
-
-	// Reset des Spielfelds, eventuell noch in FUnktion ausgliedern 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			spielfeldArray[i][j] = LEER; // 255 = ein Leerzeichen in Ascii 
-		}
-	}
-
-	// random funktion initialisieren
-	srand((unsigned int)time(NULL));
-
-	// Einführung in das Spiel und Benutzeranleitung 
-	printf("Du spielst TicTacToe, das bloedeste Spiel der Welt!\n");
-
-	// Abfrage nach dem Schwierigkeitsgrad (passende Variablen jeweils davor initialisieren)
-	// Abfragen, wer beginnen soll: 
-
-	printf("HO");
-	// Ausgabe des Spielfelds 
-	gibDasAktuelleSpielfeldAus(spielfeldArray);
-
-	bool niemandHatGewonnen = true; 
 	
-	//schleife zum spieldurchlauf, 9 durchläufe für 9 felder
-	for (int i = 0; i < 9 && niemandHatGewonnen; i++) {
-		// System clear screen einfügen
+	bool weiterspielen = true;  // Konstante Weiterspielen 
+	int spielstandSpieler = 0; 
+	int spielstandComputer = 0; 
+	int anzahlRunden = 0; 
+
+	while (weiterspielen)
+	{
+		char spielfeldArray[3][3]; // Beinhaltet alle 9 Felder des Spielfeldes 
+		bool spielerIstDran = false; // Hier hätte ich eigentlich lieber einen boolean, mit spielerbeginnt true oder false 
+
+		// Reset des Spielfelds, eventuell noch in FUnktion ausgliedern 
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				spielfeldArray[i][j] = LEER; // 255 = ein Leerzeichen in Ascii 
+			}
+		}
+
+		// random funktion initialisieren
+		srand((unsigned int)time(NULL));
+
+		// Einführung in das Spiel und Benutzeranleitung 
+		printf("Du spielst TicTacToe, das bloedeste Spiel der Welt!\n");
+
+		// Abfrage nach dem Schwierigkeitsgrad (passende Variablen jeweils davor initialisieren):
 		
-		// neues Spielfeld ausgeben
-		gibDasAktuelleSpielfeldAus(spielfeldArray);
+		// Abfragen, wer beginnen soll: 
+		printf("Moechtest du beginnen, dann druecke x. Wenn nicht druecke einfach enter.\n"); 
+		if (getchar() == 'x') spielerIstDran = true; 
 
-		if (spielerIstDran) {
-			// Beginn des Spiels 
-			printf("Du bist an der Reihe. Setze dein erstes Kreuz. Der Computer spielt mit O.\n");
+		// Ausgabe des Spielfelds 
+		gibDasAktuelleSpielfeldAus(spielfeldArray, anzahlRunden, spielstandComputer, spielstandSpieler);
 
-			int erstesKreuzZeile = 0;	// 
-			int erstesKreuzSpalte = 0; 
+		bool niemandHatGewonnen = true;
 
-			// In eine While Schleife, die überprüft, ob das Feld schon getippt wurde
-			bool belegt = true; 
-			while (belegt) {
-				belegt = false; 
-				erstesKreuzZeile = (einlesenEinerZahl("Gib nun deine Zahl für die Zeile ein", 1, 3) - 1);
-				erstesKreuzSpalte = (einlesenEinerZahl("Gib nun deine Zahl für die Spalte ein", 1, 3) - 1);
-				belegt = feldIstBelegt(spielfeldArray, erstesKreuzZeile, erstesKreuzSpalte); 
-				if (belegt) {
-					printf("Dieses Feld ist bereits belegt.\n"); 
+		//schleife zum spieldurchlauf, 9 durchläufe für 9 felder
+		for (int i = 0; i < 9 && niemandHatGewonnen; i++) {
+			system("cls"); 
+
+			// neues Spielfeld ausgeben
+			gibDasAktuelleSpielfeldAus(spielfeldArray, anzahlRunden, spielstandComputer, spielstandSpieler);
+
+			if (spielerIstDran) {
+				// Beginn des Spiels 
+				printf("Du bist an der Reihe. Setze dein erstes Kreuz. Der Computer spielt mit O.\n");
+
+				int erstesKreuzZeile = 0;	// 
+				int erstesKreuzSpalte = 0;
+
+				// In eine While Schleife, die überprüft, ob das Feld schon getippt wurde
+				bool belegt = true;
+				while (belegt) {
+					belegt = false;
+					erstesKreuzZeile = (einlesenEinerZahl("Gib nun deine Zahl für die Zeile ein", 1, 3) - 1);
+					erstesKreuzSpalte = (einlesenEinerZahl("Gib nun deine Zahl für die Spalte ein", 1, 3) - 1);
+					belegt = feldIstBelegt(spielfeldArray, erstesKreuzZeile, erstesKreuzSpalte);
+					if (belegt) {
+						printf("Dieses Feld ist bereits belegt.\n");
+					}
 				}
-			}
-			spielfeldArray[erstesKreuzZeile][erstesKreuzSpalte] = 'X'; // 88 in Ascii = X 
+				spielfeldArray[erstesKreuzZeile][erstesKreuzSpalte] = 'X'; // 88 in Ascii = X 
 
-			/*// nur zum Überprüfen da 
-			printf("\n"); 
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					printf("%c, ", spielfeldArray[i][j]);
+				/*// nur zum Überprüfen da
+				printf("\n");
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						printf("%c, ", spielfeldArray[i][j]);
+					}
 				}
+				*/
 			}
-			*/
-		}
-		else {
-			// spieler ist 0, funktion für computer zug aufrufen
-			printf("Der Computer spielt jetzt\n");
+			else {
+				// spieler ist 0, funktion für computer zug aufrufen
+				printf("Der Computer spielt jetzt\n");
 
-			// hier verschiedene Möglichkeiten, für verschiedene Schwierigkeitsgrade
-			einfacherComputergegner(spielfeldArray);
-		}
+				// hier verschiedene Möglichkeiten, für verschiedene Schwierigkeitsgrade
+				einfacherComputergegner(spielfeldArray);
+			}
 
-		// Überprüfen, ob es einen Gewinner gibt oder Gleichstand ist 
-		//if (i > 1) {
+			// Überprüfen, ob es einen Gewinner gibt oder Gleichstand ist 
 			niemandHatGewonnen = gewinnErmitteln(spielfeldArray);
-			printf("%d", niemandHatGewonnen); 
+			printf("%d", niemandHatGewonnen);
 			if (niemandHatGewonnen == false) {
-				gibDasAktuelleSpielfeldAus(spielfeldArray); 
+				anzahlRunden++;
+				// für die Optik 
+				if (spielerIstDran) spielstandSpieler++;
+				else spielstandComputer++;
+				system("cls"); 
+				gibDasAktuelleSpielfeldAus(spielfeldArray, anzahlRunden, spielstandComputer, spielstandSpieler);
 				if (spielerIstDran) printf("Der Spieler hat gewonnen.\n");
 				else printf("Der Computer hat gewonnen\n");
-				// Hier muss noch überprüft werden, wer gewonnen hat 
 			}
-		//}
-		if (i == 8 && niemandHatGewonnen) {
-			printf("Gleichstand, niemand hat gewonnen!\n");
-		}
 
-		// Wechseln des aktuellen Spielers 
-		if (spielerIstDran == true) {
-			spielerIstDran = false;
+			if (i == 8 && niemandHatGewonnen) {
+				anzahlRunden++; 
+				printf("Gleichstand, niemand hat gewonnen!\n");
+				spielstandComputer++;
+				spielstandSpieler++; 
+			}
+
+			// Wechseln des aktuellen Spielers 
+			if (spielerIstDran == true) {
+				spielerIstDran = false;
+			}
+			else {
+				spielerIstDran = true;
+			}
+		}
+		// Anweisung zum Weiterspielen bzw. Beenden des Spiels
+		printf("Wenn du nicht mehr weitermachen willst, druecke 'x' und Enter. Sonst nur Enter.\n");
+
+		//Beenden des Spiels 
+		if (getchar() == 'x') {
+			weiterspielen = false;
+			printf("Auf Wiedersehen!");
+			return 0; 
 		}
 		else {
-			spielerIstDran = true; 
+			system("cls");
 		}
 	}
-	
-
-	return 0;
 }
 
 short einlesenEinerZahl(
@@ -178,7 +204,28 @@ void einfacherComputergegner(char spielfeld[3][3]) {
 	spielfeld[zeile][spalte] = 'O';
 }
 
-void gibDasAktuelleSpielfeldAus(char spielfeld[3][3]) {
+/*void schwererComputer(char spielfeld[3][3], bool aktuellerSpieler) {
+	bool dieMitteIstNichtFrei = feldIstBelegt(spielfeld, 2, 2);
+	if (dieMitteIstNichtFrei == false) {
+		  spielfeld[2][2] = 'O';
+  }
+	int zeile, spalte;
+	bool freiesFeld = true;
+
+	while (freiesFeld) {
+		zeile = rand() % 3;
+		spalte = rand() % 3;
+		printf("\n%d", zeile);
+		printf("\n%d", spalte);
+
+		freiesFeld = feldIstBelegt(spielfeld, zeile, spalte);
+	}
+	spielfeld[zeile][spalte] = 'O';
+}
+*/
+
+void gibDasAktuelleSpielfeldAus(char spielfeld[3][3], int anzahlRunden, int spielstandComputer, int spielstandMensch) {
+	printf("Der aktuelle Spielstand nach der %d Runde ist: Computer: %d Mensch: %d\n", anzahlRunden, spielstandComputer, spielstandMensch);
 	printf("\t  1   2   3\n");
 	printf("\t%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 205, 205, 203, 205, 205, 205, 203, 205, 205, 205, 187); // macht folgende Ausgabe mit ASCII-Zeichen: ╔═══╦═══╦═══╗
 	printf("1\t%c %c %c %c %c %c %c\n", 186, spielfeld[0][0], 186, spielfeld[0][1], 186, spielfeld[0][2], 186);
